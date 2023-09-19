d31093a8fbaf9963af4a7fcf1a6f76dd9712e2dc
Martin Hanák <martinhanak97@gmail.com>
2023-09-19 20:03:34 +0200

    Custom room type MUI toggle button

--- src/routes/homepage/components/RoomTypeSelection.tsx
+++ src/routes/homepage/components/RoomTypeSelection.tsx
@@ -1,10 +1,34 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { ToggleButton, ToggleButtonGroup, styled } from "@mui/material";
 import { RoomType } from "../../../types/room";
+import { deepOrange } from "@mui/material/colors";
 
 interface RoomTypeSelection {
     selected: RoomType[],
     setSelection: (event: React.MouseEvent<HTMLElement>, input: RoomType[]) => void;
 }
+
+const RoomTypeToggleButton = styled(ToggleButton)({
+    '&.MuiToggleButtonGroup-grouped': {
+        backgroundColor: deepOrange[800],
+        borderRadius: '50px',
+        '&:Mui-disabled': {
+            backgroundColor: deepOrange[100]
+        },
+        '&.Mui-selected': {
+            backgroundColor: deepOrange[800]
+        },
+        '&:hover': {
+            backgroundColor: deepOrange[800]
+        },
+        '&:not(:last-of-type)': {
+            borderRadius: '50px',
+        },
+        '&:not(:first-of-type)': {
+            borderRadius: '50px',
+        }
+    }
+
+});
 
 export default function RoomTypeSelection({ selected, setSelection }: RoomTypeSelection) {
 
@@ -15,13 +39,13 @@ export default function RoomTypeSelection({ selected, setSelection }: RoomTypeSe
     };
 
     return (
-        <ToggleButtonGroup value={selected} onChange={setSelection} aria-label="room type selection">
+        <ToggleButtonGroup value={selected} onChange={setSelection} fullWidth aria-label="room type selection" sx={{ borderRadius: 0 }}>
 
             {Object.entries(typeToDisplayedText).map(([type, displayText]) => {
                 return (
-                    <ToggleButton key={type} value={type} aria-label={type}>
+                    <RoomTypeToggleButton key={type} value={type} aria-label={type} >
                         {displayText}
-                    </ToggleButton>
+                    </RoomTypeToggleButton>
                 );
             })}
 
