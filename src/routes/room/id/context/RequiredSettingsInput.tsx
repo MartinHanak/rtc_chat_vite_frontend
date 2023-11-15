f131f076840485d23ac24971dec324b629b34d31
Martin Hanák <martinhanak97@gmail.com>
2023-11-15 20:54:49 +0100

    Fix required settings input

--- src/routes/room/id/context/RequiredSettingsInput.tsx
+++ src/routes/room/id/context/RequiredSettingsInput.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Button, Card, Container, TextField, Typography } from "@mui/material";
 import { useLocalSettingsContext } from "../../../components/LocalSettingsContext";
 import { FormEvent, useState } from "react";
+import HeaderFiller from "../../../../components/HeaderFiller";
 
 export function RequiredSettingsInput() {
 
@@ -15,13 +16,36 @@ export function RequiredSettingsInput() {
     };
 
     return (
-        <Box component="form" onSubmit={handleSubmit}>
+        <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: "center" }}>
+            <HeaderFiller />
+            <Typography variant="h3" fontWeight={700} sx={{
+                paddingY: 2,
+                paddingX: 4,
+            }}>
+                Select a username
+            </Typography>
 
-            <TextField id="username-input" label="Username" variant="outlined"
-                value={inputUsername} onChange={(e) => setInputUsername(e.target.value)} />
+            <Card
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                autoComplete="off"
+                sx={{
+                    padding: 4,
+                    width: '100%',
+                    maxWidth: '480px',
+                    marginBottom: '360px',
+                    boxShadow: theme => theme.shadows[5],
+                    display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center',
+                    gap: 4,
+                    backgroundColor: theme => theme.palette.background.secondaryDefault
+                }}
+            >
+                <TextField sx={{ width: '100%', marginX: 4, marginY: 2 }} id="username-input" label="Username" variant="outlined"
+                    value={inputUsername} onChange={(e) => setInputUsername(e.target.value)} />
 
-            <Button variant="contained" type="submit">Confirm</Button>
-
-        </Box>
+                <Button sx={{ alignSelf: 'end', marginRight: 4 }} variant="contained" type="submit">Confirm</Button>
+            </Card>
+        </Container>
     );
 }
